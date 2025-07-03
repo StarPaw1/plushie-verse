df08b921411d33c0036f2cfe21f4a9d45b00a978
StarPaw1 <E63018@outlook.com>
2025-07-03 18:50:16 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
+let team;
+function preload(){
+team = loadImage('Team-photo.png');
+
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
-  
+  image(team,500,300);
   
 }
