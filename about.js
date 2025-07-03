e7699cd54327d0f5d24377a3e32f1f56f65ef85b
StarPaw1 <E63018@outlook.com>
2025-07-03 14:05:55 -0500

    Update about.js

--- about.js
+++ about.js
@@ -1,9 +1,15 @@
+let brodyImg;
+
+function preload() {
+  brodyImg = loadImage("brody card.png"); // file name with space
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
   fill(150, 100, 255);
   
-
+image(brodyImg,100,100,288,400);
   
   
   
