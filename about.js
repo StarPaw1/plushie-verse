2c34747fa166587f071977a6de8870639c53bfb3
StarPaw1 <E63018@outlook.com>
2025-07-04 14:44:44 -0500

    Update about.js

--- about.js
+++ about.js
@@ -2,11 +2,15 @@ let brodyImg;
 let mewbug;
 let skitters;
 let tiny;
+let scrumples;
+let dabney;
 function preload() {
   brodyImg = loadImage("brody-card.png"); // file name with space
   mewbug = loadImage("Mewbug-card.png");
   skitters = loadImage("Skitters-card.png");
   tiny = loadImage("tiny-card.png");
+  scrumples = loadImage("scrumples-card.png");
+  dabney = loadImage("dabney-card.png");
 }
 let BrodyAbout="Brody loves to bust baddies get into action!";
 
@@ -24,11 +28,15 @@ image(brodyImg,100,100,288,400);
 image(mewbug,488,100,288,400);
 image(skitters,876,100,288,400);
 image(tiny,100,700,288,400);
+image(scrumples,488,700,288,400);
+image(dabney,876,700,288,400);
   textSize(30);
   text(BrodyAbout,100,550,300);
   text("Mewbug loves making potions.She uses rare elements to make them",488,550,300);
   text("The wild card... You never know where he will pop up next!",876,550,300);
   text("Tiny may be small but is always in action with her sonic blasts!",100,1150,300);
+  text("The sky view, Scrumples loves cruising in the clouds.",488,1150,300);
+  text("The new recuit. She plays awesome tunes with her magic flute.",876,1150,300);
   
 
   
