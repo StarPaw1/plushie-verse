54a82640e4a4a1bde70988f4add42e5a23c9d7a8
StarPaw1 <E63018@outlook.com>
2025-07-04 16:00:53 -0500

    Update sketch.js

--- games/sketch.js
+++ games/sketch.js
@@ -1,1 +1,19 @@
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+  background("#fdf6ff");
+
+  if (currentScreen === "title") {
+    drawTitleScreen();
+  } else if (currentScreen === "mix") {
+    drawMixScreen();
+  } else if (currentScreen === "result") {
+    drawResultScreen();
+  }
+}
+function drawTitleScreen(){
+
+
+};
