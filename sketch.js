4b3cf41eca8bcaa15904295b1aab30ec36e1aca7
StarPaw1 <E63018@outlook.com>
2025-07-03 19:09:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,6 @@ team = loadImage('Team-photo.png');
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
-  image(team,100,100,500,500);
+  image(team,300,50,500,500);
   
 }
