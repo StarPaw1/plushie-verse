500943fd7bfac41b8f72943ecafd22dbe59cf981
StarPaw1 <E63018@outlook.com>
2025-07-09 09:53:24 -0500

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,1 +1,18 @@
 
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDXCjpBW0cPgCniJo2HH4lMVXTu2g9_c20",
+  authDomain: "plushieverse-585e1.firebaseapp.com",
+  projectId: "plushieverse-585e1",
+  storageBucket: "plushieverse-585e1.appspot.com",
+  messagingSenderId: "922619785908",
+  appId: "1:922619785908:web:003751bd980698215d9de1",
+  measurementId: "G-R3DRW3Z7B0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Set up Firestore reference
+const db = firebase.firestore();
