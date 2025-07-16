47da81b5a9a198ac9d42dd7f004a39ae0f97d1a3
StarPaw1 <E63018@outlook.com>
2025-07-16 15:29:19 -0500

    Images 1 and 2 are working and aproved by microsoft copilot
    `This is from co head Brody

--- blog.html/home.js
+++ blog.html/home.js
@@ -1,8 +1,17 @@
 let blogImg1;
 let blogImg2;
- 
+ function preload(){
+blogImg1= loadImage("blog-1.png");
+blogImg2=loadImage("blog-2.png");
+ }
 function setup(){
 createCanvas (windowWidth,windowHeight*6);
 rectMode(CORNER);
-
+drawBlogHome();
  }
+function drawBlogHome(){
+image(blogImg1,100,250,336,192);
+image(blogImg2,536,250,336,192);
+
+
+}
