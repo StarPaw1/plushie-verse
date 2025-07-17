c8e1476be5d9ac7e13c647893a266554d13662f1
StarPaw1 <E63018@outlook.com>
2025-07-17 17:46:33 -0500

    Update home.js

--- blog.html/home.js
+++ blog.html/home.js
@@ -10,8 +10,8 @@ rectMode(CORNER);
 drawBlogHome();
  }
 function drawBlogHome(){
-image(blogImg1,50,200,336*2,192*2);
-image(blogImg2,536+50,200,336*2,192*2);
+image(blogImg1,50,200,336*1.5,192*1.5);
+image(blogImg2,536+100,200,336*1.5,192*15);
 
 
 }
