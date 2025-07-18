911551052b4fc57749a010a633f8ea0666ca928c
StarPaw1 <E63018@outlook.com>
2025-07-17 20:16:33 -0500

    Update post1.js

--- blog.html/post1.js
+++ blog.html/post1.js
@@ -1,1 +1,13 @@
+let blogPara1 ="Hello! My name is StarPaw. You're probably thinking 'who is this Stapaw',well I am the programmer of this website 😉. I am also the leader of PAWSPARK, I know you are thinking PAw what now, well I will tell you all about me and PAWSPARK!";
 
+function setup(){
+createCanvas(windowWidth,windowHeight*1.5);
+textAlign(LEFT, TOP);
+
+
+};
+function post1(){
+text(blogPara1,150,200,windowWidth/0.75);
+
+
+}
