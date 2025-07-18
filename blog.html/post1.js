6f3513283f76ef6ab2405f619d43f6a0093d8150
StarPaw1 <E63018@outlook.com>
2025-07-18 15:49:41 -0500

    Update post1.js

--- blog.html/post1.js
+++ blog.html/post1.js
@@ -1,5 +1,6 @@
 let blogPara1 ="Hello! My name is StarPaw. You're probably thinking 'who is this StarPaw',well I am the programmer of this website 😉. I am also the leader of PAWSPARK, I know you are thinking PAW what now, well I will tell you all about me and PAWSPARK!";
 let me;
+let blogPara2 = "PAWSPARK is team of super plushies that help out those in need in Aqualand. Aqualand a city with many lakes and  ponds. On Aqualand's streets you can find plushies walking around and doing daily things like watering their gardens or going to the store. In total there are 50 ponds in aqualand 10 are hidden and only I can find them. "
 function preload(){
 me = loadImage("me.png");
 
@@ -16,5 +17,5 @@ function post1(){
   fill(166,196,241);
 text(blogPara1,windowWidth* 0.1,150,windowWidth*0.75);
 image(me,windowWidth*0.1+windowWidth*0.45,240+125,336,384);
-
+text(blogPara2,windowWidth* 0.1,450,windowWidth*0.75);
 }
