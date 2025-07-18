3b8b87a676ce2600e0b5f81bffce65dbe7823c27
StarPaw1 <E63018@outlook.com>
2025-07-18 13:50:18 -0500

    Update post1.js

--- blog.html/post1.js
+++ blog.html/post1.js
@@ -1,9 +1,10 @@
-let blogPara1 ="Hello! My name is StarPaw. You're probably thinking 'who is this Stapaw',well I am the programmer of this website 😉. I am also the leader of PAWSPARK, I know you are thinking PAw what now, well I will tell you all about me and PAWSPARK!";
+let blogPara1 ="Hello! My name is StarPaw. You're probably thinking 'who is this StaPaw',well I am the programmer of this website 😉. I am also the leader of PAWSPARK, I know you are thinking PAw what now, well I will tell you all about me and PAWSPARK!";
 
 function setup(){
 createCanvas(windowWidth,windowHeight*1.5);
 textAlign(LEFT, TOP);
-
+textSize(20); // Optional: Makes it more readable
+  post1();  
 
 };
 function post1(){
