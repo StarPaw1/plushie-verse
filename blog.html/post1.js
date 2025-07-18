ff17d74b1c2a751f09f15a043ccdd87ba50ccd1b
StarPaw1 <E63018@outlook.com>
2025-07-18 18:30:29 -0500

    Update post1.js

--- blog.html/post1.js
+++ blog.html/post1.js
@@ -2,10 +2,10 @@ let blogPara1 ="Hello! My name is StarPaw. You're probably thinking 'who is this
 let me;
 let blogPara2 = "PAWSPARK is team of super plushies that help out those in need in Aqualand. Aqualand a city with many lakes and  ponds. On Aqualand's streets you can find plushies walking around and doing daily things like watering their gardens or going to the store. In total there are 50 ponds in aqualand 10 are hidden and only I can find them. ";
 let blogPara3 = "    Hey I have been talking about PAWSPARK but I have never really introduced them. Well then I have to tell you all about this amazing team. First up the CO-Head Brody D. a dinosaur squishmallow, He loves hot cocoa and is really strong. Next up second in command after the CO Head Mewbug the Potion maker she makes potions that are purrfect for her crazy vibe she is a green cat, also in love with the next member. Next up the wild card Skitters he is super fast you never know where he will pop up next he is a dog and is also in love with Mewbug. Next make sure you never underestimate Tiny she makes super sonic frequencies that will blow you away, she is a small owl. Next we have the eye in the sky Scrumples, she is a pink bunny. Next our musician, Dabney the artic fox who brings the frost. Last but not least Starfluff the super smart polar bear. ";
-
+let backG;
 function preload(){
 me = loadImage("me.png");
-
+backG = loadImage("blog-background.png");
 };
 function setup(){
 createCanvas(windowWidth,windowHeight*4.5);
@@ -15,7 +15,7 @@ textSize(35); // Optional: Makes it more readable
 
 };
 function post1(){
-
+image(backG,0,0,windowWidth,windowHeight*4.5);
   fill(166,196,241);
 text(blogPara1,windowWidth* 0.1,150,windowWidth*0.75);
 image(me,windowWidth*0.1+windowWidth*0.45,240+125,336,384);
