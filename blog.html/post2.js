fe19d44751335aaf7e1a767942b2d5b86881ed32
StarPaw1 <E63018@outlook.com>
2025-07-23 13:20:30 -0500

    Update post2.js

--- blog.html/post2.js
+++ blog.html/post2.js
@@ -1,4 +1,4 @@
-let diarypara1 = "Today we got extra superpowers not like the ones in Marvel. Different like from the unierse. Do you Know which. We got star power!";
+let diarypara1 = "Today we got extra superpowers not like the ones in Marvel. Different like from the universe. Do you Know which. We got star power!";
 let diarypara2 = "Star power many have not heard of such. But we got them. Why us? Why not someone else. At the time we had many juniors how were me and Regan supposed to manage this.";
 let diaryPara3 = "At the the first minute the 5 of us were able to cry 5 gallons. I wonder if Regan felt the same way. We had gotten two new members, Mewbug, and Skitters.";
 
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
   background(20,10,40,30); // semi-transparent background for trailing effect
-
+ diary();
   // Star core
   fill(255, 220, 250);
   ellipse(width / 2, height / 2, 50 + sin(frameCount * 0.1) * 10);
@@ -29,9 +29,10 @@ function draw() {
 
 }
 function diary (){
-  fill(255); // White text
-textSize(18);
+  fill(100,255,220); // White text
+textSize(40);
   background(255,255,255);
 background(20,10,40,30); // semi-transparent background for trailing effect
-   text(diarypara1,50,50,windowWidth*0.60);
+   text(diarypara1,50,50,windowWidth*0.40);
+  text(diarypara2,60,200);
 };
