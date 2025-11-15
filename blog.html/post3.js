bdb1757635f5b71440302ff1e63f6ade64cb78cd
StarPaw1 <E63018@outlook.com>
2025-11-15 16:06:23 -0600

    i'm good
    u can style me

--- blog.html/post3.js
+++ blog.html/post3.js
@@ -1,4 +1,11 @@
-
+let bloggin = false;
 document.getElementById("depressed").addEventListener("click", function() {
   document.getElementById("depressed").style.color = "rgb(40, 244, 99)";
+   const element = document.getElementById("depressed");
+  
+  element.style.color = "rgb(40, 244, 99)";
+  bloggin = true;
+  if (bloggin) {
+  element.style.backgroundColor = "rgb(40, 24, 235)";
+}
 });
